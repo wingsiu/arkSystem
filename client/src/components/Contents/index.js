eb9aeced4f9b8c92c79a1edb62f1d44930b672b9
wingsiu <wingsiu48@gmail.com>
2018-08-06 12:39:33 +0800

    antDPro as client UI

--- client/src/components/Contents/index.js
+++ client/src/components/Contents/index.js
@@ -5,13 +5,20 @@
  */
 
 import React from 'react';
+import withStyles from "@material-ui/core/styles/withStyles";
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+
 
 function Contents(props) {
-  return (
-    <div>
+const { classes, ...rest } = props;
+
+    return (
+    <div className={classes.container}>
+    <h3 className={classes.subtitle}>
       <p>The page you're looking for {window.location.pathname}.</p>
+    </h3>
     </div>
   );
 }
 
-export default Contents;
+export default withStyles(componentsStyle)(Contents);
--- client/src/components/NotFoundPage/index.js
+++ client/src/components/NotFoundPage/index.js
@@ -5,13 +5,18 @@
  */
 
 import React from 'react';
+import withStyles from "@material-ui/core/styles/withStyles";
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
 
 function NotFoundPage(props) {
+const { classes, ...rest } = props;
   return (
-    <div>
+    <div className={classes.container}>
+        <h3 className={classes.subtitle}>
       <p>The page you're looking for doesn't exist.</p>
+        </h3>
     </div>
   );
 }
 
-export default NotFoundPage;
+export default withStyles(componentsStyle)(NotFoundPage);
